8e58f07c17f50ab6971aeb3bef0e73346e0cc763
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-06 17:01:31 +0200

    laurels done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={ images.chef } alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="chef&apos;s word" />
+      <h1 className='headtext__cormorant'>what we blieve in</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_qout'>
+          <img src={ images.quote } alt="qout" />
+          <p className='p__opensans'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
+          </p>
+        </div>
+        <p className='p__opensans'>
+          auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.
+        </p>
+      </div>
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={ images.sign } alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
